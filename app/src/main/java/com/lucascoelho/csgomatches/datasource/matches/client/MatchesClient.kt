aac67e1356de1bd18d970111d927c4533b09deab
Coelho <lcsilva2099@gmail.com>
2023-03-19 20:47:32 -0300

    Hiding api key

--- app/src/main/java/com/lucascoelho/csgomatches/datasource/matches/client/MatchesClient.kt
+++ app/src/main/java/com/lucascoelho/csgomatches/datasource/matches/client/MatchesClient.kt
@@ -1,5 +1,6 @@
 package com.lucascoelho.csgomatches.datasource.matches.client
 
+import com.lucascoelho.csgomatches.BuildConfig
 import com.lucascoelho.csgomatches.datasource.matches.entities.Teams
 import com.lucascoelho.csgomatches.datasource.matches.entities.response.MatchResponse
 import retrofit2.http.GET
@@ -9,12 +10,12 @@ interface MatchesClient {
 
     @GET("csgo/matches")
     suspend fun getAllMatches(
-        @Query("token") token: String = ""
+        @Query("token") token: String = BuildConfig.API_KEY
     ): List<MatchResponse>
 
     @GET("csgo/teams")
     suspend fun getTeamDetails(
-        @Query("token") token: String = "",
+        @Query("token") token: String = BuildConfig.API_KEY,
         @Query("filter[id]") id: Int
     ): List<Teams>
 }
