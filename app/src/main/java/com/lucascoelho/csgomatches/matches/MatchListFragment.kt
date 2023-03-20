8e9d2c140e9c393f453ba29bcf802753664523d6
Coelho <lcsilva2099@gmail.com>
2023-03-19 23:09:10 -0300

    Toast when teams are not defined

--- app/src/main/java/com/lucascoelho/csgomatches/matches/MatchListFragment.kt
+++ app/src/main/java/com/lucascoelho/csgomatches/matches/MatchListFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -30,6 +31,12 @@ class MatchListFragment : Fragment() {
             } catch (ignored: Exception) {
                 // TODO: Tratar exceção
             }
+        } else {
+            Toast.makeText(
+                requireContext(),
+                "Os times desta partida ainda não foram definidos.",
+                Toast.LENGTH_LONG
+            ).show()
         }
     }
 
